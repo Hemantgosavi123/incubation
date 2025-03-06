17d33c2d957e77b10cc40e876a77ac806458680c
Hemantgosavi123 <hemantgosavinew@gmail.com>
2025-03-06 20:27:31 +0530

    Encapsulation Examples

---
+++ JspiderBootCamp/a_EclipseJavaProject/a_JAVA/a2_OOPs/Day2_PrivateVarAccess/Company.java
@@ -0,0 +1,49 @@
+package Day2_PrivateVarAccess;
+
+class Company {
+    public static void main(String args[])
+    {
+        Employee emp1 = new Employee(10,"Siddhu",45000);
+        System.out.println("id = "+emp1.emp_id);
+        System.out.println("name = "+emp1.emp_name);
+        // System.out.println("sal = "+emp1.emp_sal);	//you can not access private variable directly
+        System.out.println("sal = "+emp1.getSal()); 	//using getter method you can access the salary
+
+        System.out.println("++++++++++++++++++++++++++++++");
+
+        Employee emp2 = new Employee(11,"Dinu",70000);
+        System.out.println("id = "+emp2.emp_id);
+        System.out.println("name = "+emp2.emp_name);
+        // System.out.println("sal = "+emp2.emp_sal);	//you can not modify private variable directly
+        System.out.println("sal = "+emp2.getSal());		//using setter method you can modify the salary
+
+        double i = emp2.getSal()*1.25;	//trying to getting increment in salary
+        System.out.println("I = = "+i);
+        emp2.setSal(80000);	//modifying salary for emp2 using setter method
+        System.out.println("Salary Increment = "+emp2.getSal());
+    }    
+}
+
+class Employee{
+    int emp_id;
+    String emp_name;
+    private double emp_sal;
+
+    public double getSal() //getter method
+    {
+        return emp_sal;
+    }
+
+    public void setSal(double emp_sal)
+    {
+        this.emp_sal=emp_sal;
+    }
+
+    Employee(int emp_id,String emp_name,double emp_sal)
+    {
+        this.emp_id=emp_id;
+        this.emp_name=emp_name;
+        this.emp_sal=emp_sal;
+    }
+}
+
---
+++ JspiderBootCamp/a_EclipseJavaProject/a_JAVA/a2_OOPs/Day2_PrivateVarAccess/SBIBank.java
@@ -0,0 +1,31 @@
+package Day2_PrivateVarAccess;
+
+class ATM {
+    private double money=200000;    //State
+ 
+    public void withdraw()   //behaviours
+    {
+     System.out.println("You can Withdraw your Money");
+    }
+
+    public void setMethod(double money)
+    {
+     this.money=money;
+    }
+    public double getMethod()
+    {
+         return this.money;
+    }
+
+    
+}
+
+class SBIBank{      //Driver Class
+ public static void main(String args[])
+ {
+     ATM ref = new ATM();
+     ref.setMethod(28999);        
+     System.out.println("Total Cash = "+ref.getMethod());
+     ref.withdraw();
+ }
+}
